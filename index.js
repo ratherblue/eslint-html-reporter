9c4cd5e9830f674089aa45bc22c511f40d8039f6
edendramis <evangeliad@gmail.com>
2015-03-27 14:03:04 -0700

    testing

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var handlebars = require('handlebars');
 var fs = require('fs');
+var path = require('path');
 
 module.exports = function (results, config) {
   'use strict';
 
   // fetch and compile template
-  var source = fs.readFileSync('./templates/reporter.hbs',
+  var source = fs.readFileSync(path.join(__dirname, 'reporter.hbs'),
     { encoding: 'utf-8' }
   );
 
@@ -24,11 +25,11 @@ module.exports = function (results, config) {
     }
   );
 
-  console.log(data);
+  //console.log(data);
 
   var html = template(JSON.parse(data));
 
-  fs.writeFileSync('./report.html', html);
+  //fs.writeFileSync('./report.html', html);
 
   return template(JSON.parse(data));
 };
