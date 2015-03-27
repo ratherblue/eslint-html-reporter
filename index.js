2aa2eecda4253c02843f81bea17548d28fa1df77
edendramis <evangeliad@gmail.com>
2015-03-27 13:49:56 -0700

    Adding preliminary files

---
+++ index.js
@@ -0,0 +1,34 @@
+var handlebars = require('handlebars');
+var fs = require('fs');
+
+module.exports = function (results, config) {
+  'use strict';
+
+  // fetch and compile template
+  var source = fs.readFileSync('./templates/reporter.hbs',
+    { encoding: 'utf-8' }
+  );
+
+  var template = handlebars.compile(source);
+
+  var data = JSON.stringify(
+    {
+      config: config,
+      results: results
+    },
+    function (key, val) {
+      // filter away the Esprima AST
+      if (key !== 'node') {
+        return val;
+      }
+    }
+  );
+
+  console.log(data);
+
+  var html = template(JSON.parse(data));
+
+  fs.writeFileSync('./report.html', html);
+
+  return template(JSON.parse(data));
+};
